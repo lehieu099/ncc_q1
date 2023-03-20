5a69bde42d8c6f700d33642fd09c93e22d06f2d3
lehieu099 <duyhieu099a2@gmail.com>
2023-03-20 15:51:29 +0700

    [hieuld-202303]:update post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ncc_q1';
+
+  textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique consequat placerat. Vestibulum auctor pellentesque sem, eu posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam facilisis, ligula in mattis sodales, augue justo tristique nulla, sed lacinia ante eros ut mi. Morbi vitae diam augue. Aliquam vel mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend convallis. Quisque suscipit maximus vestibulum. Phasellus congue mollis orci, sit amet luctus augue tristique eu. Donec vulputate odio neque, sed semper turpis pellentesque a.";
+  img1 = "../assets/images/css-icon 1.png";
+  img2 = "../assets/images/html-icon 1.png";
+  img3 = "../assets/images/url-icon 1.png";
+
+  textTitle = "Lorem ipsum dolor sit asmet?"
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   constructor() { }
-
+  @Input() textContent: any;
+  @Input() img: any;
+  @Input() textTitle: any;
   ngOnInit(): void {
   }
 
